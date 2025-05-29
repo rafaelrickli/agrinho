cdd4d460c6fc5de0731bbc8c8f2a6fee06f54c4a
rafaelrickli <rafael.rickli@escola.pr.gov.br>
2025-05-29 11:23:19 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,4 +4,21 @@ function setup() {
 
 function draw() {
   background(220);
+   fill("green")
+  rect(0,60,400,270);
+fill("yellow")
+  triangle(60,200, 200, 90, 200,300);
+  
+fill("yellow")
+  triangle(340,200, 200, 90, 200,300);
+fill("blue")
+ circle(200, 200, 100);
+  fill("white");
+  rect(150, 196, 100,10)
+let palavra = "desordem e regresso"
+  fill("black")
+  textSize(10)
+  textAlign(CENTER, CENTER);
+  text(palavra, 200, 200);
+  
 }
